046e91a116ed863bd53126da0459e7054f101ba7
hhd <504192077@qq.com>
2021-07-05 23:17:43 +0800

    add tabs组件及路由配置

---
+++ src/views/recommend.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="search">
+    推荐页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'recommend'
+}
+</script>
---
+++ src/views/singer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="search">
+    歌手页面
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'singer'
+}
+</script>
