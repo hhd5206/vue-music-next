80e0d9cd1b3763ef8ffe682dd35133716c1c9469
hhd <504192077@qq.com>
2021-07-08 11:00:10 +0800

    推荐页编写及歌手页初步开发

---
+++ src/components/base/scroll/scroll.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="rootRef">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import useScroll from './use-scroll'
+export default {
+  name: 'scroll',
+  props: {
+    click: {
+      type: Boolean,
+      default: true
+    },
+    probeType: {
+      type: Number,
+      default: 0
+    }
+  },
+  setup(props) {
+    const rootRef = ref(null)
+    useScroll(rootRef, props)
+    return {
+      rootRef
+    }
+  }
+}
+</script>
--- src/views/singer.vue
+++ src/views/singer.vue
@@ -1,11 +1,34 @@
 <template>
-  <div class="search">
-    歌手页面
+  <div class="singer" v-loading="!singers.length">
+    <index-list :data="singers"></index-list>
   </div>
 </template>
 
 <script>
+import { getSingerList } from '@/server/singer'
+import IndexList from '@/components/base/index-list/index-list'
 export default {
-  name: 'singer'
+  name: 'singer',
+  components: {
+    IndexList
+  },
+  data() {
+    return {
+      singers: []
+    }
+  },
+  async created() {
+    const result = await getSingerList()
+    this.singers = result.singers
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+.singer {
+  position: fixed;
+  width: 100%;
+  top: 88px;
+  bottom: 0;
+}
+</style>
